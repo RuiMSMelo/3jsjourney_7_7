a6f9859d39dcfae69ab86feef8d595cb4a4bd2bd
RuiMSMelo <ruimsmelo@gmail.com>
2025-05-27 13:37:22 +0100

    animations buttons with leva

--- src/Fox.jsx
+++ src/Fox.jsx
@@ -1,19 +1,23 @@
 import { useGLTF, useAnimations } from '@react-three/drei'
 import { useEffect } from 'react'
+import { useControls } from 'leva'
 
 export default function Fox(props) {
     const foxModel = useGLTF('./Fox/glTF/Fox.gltf')
     const animations = useAnimations(foxModel.animations, foxModel.scene)
 
-    useEffect(() => {
-        const action = animations.actions.Run
-        action.play()
+    const { animationName } = useControls('Fox', {
+        animationName: { options: animations.names },
+    })
 
-        window.setTimeout(() => {
-            animations.actions.Walk.play()
-            animations.actions.Walk.crossFadeFrom(animations.actions.Run, 1)
-        }, 2000)
-    }, [])
+    useEffect(() => {
+        const action = animations.actions[animationName]
+        action.reset().fadeIn(0.5).play()
+
+        return () => {
+            action.fadeOut(0.5)
+        }
+    }, [animationName])
 
     return (
         <>
